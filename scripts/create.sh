b0543762b2408403032892f6ffbb83ea6f1a3c6e
yangqinjiang <531553280@qq.com>
2019-01-15 11:01:11 +0800

    脚本

--- scripts/create.sh
+++ scripts/create.sh
@@ -1,22 +1,23 @@
 #将生成的创始块和通道文件存储在该目录中
 channelArtifactsDir=../channel-artifacts
 channelName=qbgoochannel
+FABRIC_CFG_PATH=$(pwd)/../
 
 # 1. 在项目根目录下创建新目录 $channelArtifactsDir, 
 # 将生成的创始块和通道文件存储在该目录中
 mkdir $channelArtifactsDir
 # 生成创始块文件
-configtxgen --configPath=../configtx.yaml -profile OrgsOrdererGenesis -outputBlock ./$channelArtifactsDir/genesis.block
+configtxgen  -profile OrgsOrdererGenesis -outputBlock ./$channelArtifactsDir/genesis.block
 
 
 # 创建channel
 # channel.tx中包含了用于生产channel的信息
-configtxgen --configPath=../configtx.yaml -profile OrgsChannel -outputCreateChannelTx ./$channelArtifactsDir/channel.tx -channelID $channelName
+configtxgen  -profile OrgsChannel -outputCreateChannelTx ./$channelArtifactsDir/channel.tx -channelID $channelName
 
 # 生成相关的锚点文件 - 组织A
-configtxgen --configPath=../configtx.yaml -profile OrgsChannel -outputAnchorPeersUpdate ./$channelArtifactsDir/OrgAMSPanchors.tx -channelID $channelName -asOrg OrgAMSP
+configtxgen  -profile OrgsChannel -outputAnchorPeersUpdate ./$channelArtifactsDir/OrgAMSPanchors.tx -channelID $channelName -asOrg OrgAMSP
 # 生成相关的锚点文件 - 组织B
-configtxgen --configPath=../configtx.yaml -profile OrgsChannel -outputAnchorPeersUpdate ./$channelArtifactsDir/OrgBMSPanchors.tx -channelID $channelName -asOrg OrgBMSP
+configtxgen  -profile OrgsChannel -outputAnchorPeersUpdate ./$channelArtifactsDir/OrgBMSPanchors.tx -channelID $channelName -asOrg OrgBMSP
 #查看$channelArtifactsDir目录下生成的文件
 tree $channelArtifactsDir/
 
