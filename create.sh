df2e663d6351d2d4c72b88368c6de7f76c5c564e
yangqinjiang <531553280@qq.com>
2019-01-15 13:30:05 +0800

    脚本

--- create.sh
+++ create.sh
@@ -15,6 +15,7 @@ CHANNEL_NAME="$1"
 
 echo "===================== '通道名称: $CHANNEL_NAME' ===================== "
 # Generates Org certs using cryptogen tool
+#使用cryptogen生成证书
 generateCerts(){
     echo "##########################################################"
     echo "################### 使用cryptogen生成证书 #################"
@@ -42,37 +43,43 @@ generateCerts(){
     echo
 }
 
+#生成的创始块和通道文件
+generateChannelArtifacts() {
+    
+    echo "===================== 生成的创始块和通道文件 ===================== "
+    #将生成的创始块和通道文件存储在该目录中
+    channelArtifactsDir=channel-artifacts
+
+
+
+    # 1. 在项目根目录下创建新目录 $channelArtifactsDir, 
+    # 将生成的创始块和通道文件存储在该目录中
+    [ ! -d  $channelArtifactsDir ] && mkdir $channelArtifactsDir
+
+    echo "===================== 创始块文件 ===================== "
+    # 生成创始块文件
+    configtxgen   -profile OrgsOrdererGenesis -outputBlock $channelArtifactsDir/${CHANNEL_NAME}_genesis.block
+
+
+    echo "===================== 创建channel.tx文件 ===================== "
+    # 创建channel
+    # channel.tx中包含了用于生产channel的信息
+    configtxgen  -profile OrgsChannel -outputCreateChannelTx ./$channelArtifactsDir/${CHANNEL_NAME}_channel.tx -channelID $CHANNEL_NAME
+
+    echo "===================== 生成相关的锚点文件 - 组织A ===================== "
+    # 生成相关的锚点文件 - 组织A
+    configtxgen  -profile OrgsChannel -outputAnchorPeersUpdate ./$channelArtifactsDir/OrgAMSPanchors.tx -channelID $CHANNEL_NAME -asOrg OrgAMSP
+    echo "===================== 生成相关的锚点文件 - 组织B ===================== "
+    # 生成相关的锚点文件 - 组织B
+    configtxgen  -profile OrgsChannel -outputAnchorPeersUpdate ./$channelArtifactsDir/OrgBMSPanchors.tx -channelID $CHANNEL_NAME -asOrg OrgBMSP
+    #查看$channelArtifactsDir目录下生成的文件
+
+    tree $channelArtifactsDir/
+}
+
 generateCerts
+generateChannelArtifacts
 
-echo "===================== 生成的创始块和通道文件 ===================== "
-#将生成的创始块和通道文件存储在该目录中
-channelArtifactsDir=channel-artifacts
-
-
-
-# 1. 在项目根目录下创建新目录 $channelArtifactsDir, 
-# 将生成的创始块和通道文件存储在该目录中
-[ ! -d  $channelArtifactsDir ] && mkdir $channelArtifactsDir
-
-echo "===================== 创始块文件 ===================== "
-# 生成创始块文件
-configtxgen   -profile OrgsOrdererGenesis -outputBlock $channelArtifactsDir/${CHANNEL_NAME}_genesis.block
-
-
-echo "===================== 创建channel.tx文件 ===================== "
-# 创建channel
-# channel.tx中包含了用于生产channel的信息
-configtxgen  -profile OrgsChannel -outputCreateChannelTx ./$channelArtifactsDir/${CHANNEL_NAME}_channel.tx -channelID $CHANNEL_NAME
-
-echo "===================== 生成相关的锚点文件 - 组织A ===================== "
-# 生成相关的锚点文件 - 组织A
-configtxgen  -profile OrgsChannel -outputAnchorPeersUpdate ./$channelArtifactsDir/OrgAMSPanchors.tx -channelID $CHANNEL_NAME -asOrg OrgAMSP
-echo "===================== 生成相关的锚点文件 - 组织B ===================== "
-# 生成相关的锚点文件 - 组织B
-configtxgen  -profile OrgsChannel -outputAnchorPeersUpdate ./$channelArtifactsDir/OrgBMSPanchors.tx -channelID $CHANNEL_NAME -asOrg OrgBMSP
-#查看$channelArtifactsDir目录下生成的文件
-
-tree $channelArtifactsDir/
 
 echo
 echo "========= All GOOD, BYFN execution completed =========== "
